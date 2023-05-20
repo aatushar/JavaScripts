5764ef75ac6c64488de73df75cf389aa56c45614
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-05-20 12:51:28 +0600

    20 may 2023
    Add

---
+++ js 2/script2.js
@@ -0,0 +1,20 @@
+// let X=50;
+// let y=20;
+// let result=X/y;
+// console.log(result);
+
+// let radius=5;
+// const pi=3.25554555;
+// let areaOfCircle=pi*radius*radius;
+// console.log(areaOfCircle);
+
+// let x=10;
+// console.log(X%=3);
+let x=7;
+let y=8;
+ 
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x!=y);
