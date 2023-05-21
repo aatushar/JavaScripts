11a3a7db7346c4edfa88da287fe06820b2f7a0d4
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-05-21 13:13:08 +0600

    21 may 2023
    Add

---
+++ js3/test-if_statement.js
@@ -0,0 +1,66 @@
+// let x=333;
+//  if (x%3==0){
+// console.log("true");
+//  }
+// else {
+//     console.log("false")
+// }
+// let y=300;
+// if (y/300); {
+//     console.log("true")
+// }
+// else {
+//     console.log("false")
+// }
+
+
+//0-18 cool, 19-27 normal, 28 to unlimited abnormal
+
+// let weather= 50;
+// if(weather>=27){
+//     console.log("abnormal weather")
+// }
+// else if(weather>=18){
+//     console.log("Normal weather")}
+
+// else if(weather>=0){
+//     console.log("cool weather")
+// }
+
+// else{
+//     console.log("Invalid weather")
+// }
+
+// let weather =40;
+// if(weather>=27){
+//     console.log("Abnormal weather");
+// }
+// else if (weather>=18) {
+//     console.log("normal weather");}
+
+// else if( weather>0) {
+//     console.log("Cool weather");
+// }
+// else {
+//     console.log("Invalid weather");
+// }
+
+// for(let i=4; i<=100; i++){
+   
+//     if(i%2==1) {
+        
+//     }
+//     else{console.log(i);
+
+//     }
+// }
+//1-50 =3 by divided and 5 divided
+
+for(i=1; i<=50; i++){
+    if( i%3==0 || i%5==0)
+    console.log(i);
+}
+else{
+
+}
+let input =5;
