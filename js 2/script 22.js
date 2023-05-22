7f7c9be76f0020bdab47606d9905726f7cd46b70
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-05-22 13:03:21 +0600

    22 may
    add

---
+++ js 2/script 22.js
@@ -0,0 +1,123 @@
+//google password & user name #1
+
+// let userName= "rajib"
+// let password="asd123"
+
+// if (userName=="rajib" )
+// if(password=="asd123") {
+//     console.log ("welcome "+userName)
+
+
+// }
+// else {
+//     console.log ("user name not found")
+
+// }
+
+
+// else {
+//     console.log("password not found");
+// }
+
+// result sheet #2
+
+// let marks = 101;
+// if (marks > 100) {
+//     console.log("invalid number")
+// }
+// else if (marks >= 80) {
+//     console.log("A+")
+
+// }
+// else if (marks >= 70) {
+//     console.log("A")
+// }
+
+
+// else if (marks >= 60) {
+//     console.log("A-")
+// }
+// else if (marks >= 50) {
+//     console.log("B")
+// }
+
+// else if (marks >= 40) {
+//     console.log("C")
+// }
+
+// else if (marks >= 33) {
+//     console.log("D")
+// }
+
+// else {
+//     console.log("Invalid Number")
+// }
+
+//Day check #3 number
+// let day = 3;
+
+// switch (day) {
+//     case 1:
+//         console.log("Saturday");
+//         break;
+//     case 2:
+//         console.log("Sunday");
+//         break;
+//     case 3:
+//         console.log("Monday");
+//         break;
+//     case 4:
+//         console.log("Tuesday");
+//         break;
+//     case 5:
+//         console.log("Wednessday");
+//         break;
+//     case 6:
+//         console.log("Thursday");
+//         break;
+//     case 7:
+//         console.log("Friday");
+//         break;
+//     default:
+//         console.log("Nothing Found")
+// }
+
+
+//prime Number #4
+// let i= 6;
+// let counter=0;
+// for(i=1;i<=Number;i++){
+//     if(number% i==0){
+//         counter =counter+1;
+//     }
+
+// }
+// if(counter==2){
+// console.log("Prime Number");
+// }
+// else{
+//     console.log("Not prime Number");
+// }
+
+
+//  swap #5
+// let x=50;
+// let y=20;
+// let temp=0;
+// temp=x;
+// x=y;
+// y=temp;
+// console.log("x= "+x+" y= "+y);
+
+//febinacci #6
+let n1=0;
+let n2=1;
+let n3=0;
+console.log(n1+" \n"+ n2);
+// for(let i=3; i<=8; i++){
+//     n3=n1+n2;
+//     console.log(n3);
+//     n1=n2;
+//     n2=n3;
+// }
+
