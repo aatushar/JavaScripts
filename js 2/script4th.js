4c27f0dea562b9a7bc3688748b41b29e5759f0eb
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-05-23 13:02:13 +0600

    java 4th class
    Add

---
+++ js 2/script4th.js
@@ -0,0 +1,70 @@
+// #1 multiplication table 
+// let n= 2;
+// let m=1;
+// for(i=1;i<=10;i++){
+//     m=n*i
+//     console.log(n+"x"+i+"="+m)
+// }
+// #2test-whil-loop
+// let n=1;
+
+// while(n<+10){
+//     console.log(n);
+//     n++;
+// }
+// #3// #test do while
+// let n=5;
+// do{
+//     console.log(n);
+//     while(n<=10);
+// }
+// #3 continuue
+// let n= 8;
+// while (n<=17){
+//     if(n==10 || n==15 ||n==16) {
+//         n++;
+//         continue;
+//     }
+//     else{
+//         console.log(n);
+//     }
+// }
+
+//#4 1-50 number which are except divided by 3 and 5
+// while loop
+// let x=1;
+// while(n<=50){
+// if(n%3==0 && n%5==0){
+
+// }
+// else{ 
+
+// }
+// n++
+
+// }
+
+// for lop
+// for(let i=1; i<=50; i++){
+//     if(i%3==0 && i%5==0)
+//     continue;
+//     else{
+//         console.log(i);
+//     }
+// }
+
+//#5 arrey 
+let names=[10]
+names[0]="Python";
+names[1]="Java";
+names[2]="c#";
+names[3]="Python";
+names[4]="Python";
+names[5]="Python";
+names[6]="Python";
+names[7]="Python";
+names[8]="Python";
+names[9]="Python";
+names[10]="Python";
+
+console.log(names)
